750cab3ca97f1e30a8cb1858fede85bb44d9f051
Felipe Coury <felipe.coury@gmail.com>
2024-02-01 12:55:18 -0300

    status line

---
+++ src/editor.rs
@@ -0,0 +1,219 @@
+use std::io::{stdout, Write};
+
+use crossterm::{
+    cursor,
+    event::{self, read},
+    style::{self, Color, Stylize},
+    terminal, ExecutableCommand, QueueableCommand,
+};
+
+enum Action {
+    Quit,
+
+    MoveUp,
+    MoveDown,
+    MoveLeft,
+    MoveRight,
+
+    AddChar(char),
+    NewLine,
+
+    EnterMode(Mode),
+}
+
+#[derive(Debug)]
+enum Mode {
+    Normal,
+    Insert,
+}
+
+pub struct Editor {
+    stdout: std::io::Stdout,
+    size: (u16, u16),
+    cx: u16,
+    cy: u16,
+    mode: Mode,
+}
+
+impl Drop for Editor {
+    fn drop(&mut self) {
+        _ = self.stdout.flush();
+        _ = self.stdout.execute(terminal::LeaveAlternateScreen);
+        _ = terminal::disable_raw_mode();
+    }
+}
+
+impl Editor {
+    pub fn new() -> anyhow::Result<Self> {
+        let mut stdout = stdout();
+        terminal::enable_raw_mode()?;
+        stdout
+            .execute(terminal::EnterAlternateScreen)?
+            .execute(terminal::Clear(terminal::ClearType::All))?;
+
+        Ok(Editor {
+            stdout,
+            cx: 0,
+            cy: 0,
+            mode: Mode::Normal,
+            size: terminal::size()?,
+        })
+    }
+
+    pub fn draw(&mut self) -> anyhow::Result<()> {
+        self.draw_statusline()?;
+        self.stdout.queue(cursor::MoveTo(self.cx, self.cy))?;
+        self.stdout.flush()?;
+
+        Ok(())
+    }
+
+    pub fn draw_statusline(&mut self) -> anyhow::Result<()> {
+        let mode = format!(" {:?} ", self.mode).to_uppercase();
+        let file = " src/main.rs";
+        let pos = format!(" {}:{} ", self.cx, self.cy);
+
+        let file_width = self.size.0 - mode.len() as u16 - pos.len() as u16 - 2;
+
+        self.stdout.queue(cursor::MoveTo(0, self.size.1 - 2))?;
+        self.stdout.queue(style::PrintStyledContent(
+            mode.with(Color::Rgb { r: 0, g: 0, b: 0 })
+                .bold()
+                .on(Color::Rgb {
+                    r: 184,
+                    g: 144,
+                    b: 243,
+                }),
+        ))?;
+        self.stdout.queue(style::PrintStyledContent(
+            ""
+                .with(Color::Rgb {
+                    r: 184,
+                    g: 144,
+                    b: 243,
+                })
+                .on(Color::Rgb {
+                    r: 67,
+                    g: 70,
+                    b: 89,
+                }),
+        ))?;
+        self.stdout.queue(style::PrintStyledContent(
+            format!("{:<width$}", file, width = file_width as usize)
+                .with(Color::Rgb {
+                    r: 255,
+                    g: 255,
+                    b: 255,
+                })
+                .bold()
+                .on(Color::Rgb {
+                    r: 67,
+                    g: 70,
+                    b: 89,
+                }),
+        ))?;
+        self.stdout.queue(style::PrintStyledContent(
+            ""
+                .with(Color::Rgb {
+                    r: 184,
+                    g: 144,
+                    b: 243,
+                })
+                .on(Color::Rgb {
+                    r: 67,
+                    g: 70,
+                    b: 89,
+                }),
+        ))?;
+        self.stdout.queue(style::PrintStyledContent(
+            pos.with(Color::Rgb { r: 0, g: 0, b: 0 })
+                .bold()
+                .on(Color::Rgb {
+                    r: 184,
+                    g: 144,
+                    b: 243,
+                }),
+        ));
+
+        Ok(())
+    }
+
+    pub fn run(&mut self) -> anyhow::Result<()> {
+        loop {
+            self.draw()?;
+            if let Some(action) = self.handle_event(read()?)? {
+                match action {
+                    Action::Quit => break,
+                    Action::MoveUp => {
+                        self.cy = self.cy.saturating_sub(1);
+                    }
+                    Action::MoveDown => {
+                        self.cy += 1;
+                    }
+                    Action::MoveLeft => {
+                        self.cx = self.cx.saturating_sub(1);
+                    }
+                    Action::MoveRight => {
+                        self.cx += 1;
+                    }
+                    Action::EnterMode(new_mode) => {
+                        self.mode = new_mode;
+                    }
+                    Action::AddChar(c) => {
+                        self.stdout.queue(cursor::MoveTo(self.cx, self.cy))?;
+                        self.stdout.queue(style::Print(c))?;
+                        self.cx += 1;
+                    }
+                    Action::NewLine => {
+                        self.cx = 0;
+                        self.cy += 1;
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn handle_event(&mut self, ev: event::Event) -> anyhow::Result<Option<Action>> {
+        if matches!(ev, event::Event::Resize(_, _)) {
+            self.size = terminal::size()?;
+        }
+
+        match self.mode {
+            Mode::Normal => self.handle_normal_event(ev),
+            Mode::Insert => self.handle_insert_event(ev),
+        }
+    }
+
+    fn handle_normal_event(&self, ev: event::Event) -> anyhow::Result<Option<Action>> {
+        let action = match ev {
+            event::Event::Key(event) => match event.code {
+                event::KeyCode::Char('q') => Some(Action::Quit),
+                event::KeyCode::Up | event::KeyCode::Char('k') => Some(Action::MoveUp),
+                event::KeyCode::Down | event::KeyCode::Char('j') => Some(Action::MoveDown),
+                event::KeyCode::Left | event::KeyCode::Char('h') => Some(Action::MoveLeft),
+                event::KeyCode::Right | event::KeyCode::Char('l') => Some(Action::MoveRight),
+                event::KeyCode::Char('i') => Some(Action::EnterMode(Mode::Insert)),
+                _ => None,
+            },
+            _ => None,
+        };
+
+        Ok(action)
+    }
+
+    fn handle_insert_event(&self, ev: event::Event) -> anyhow::Result<Option<Action>> {
+        let action = match ev {
+            event::Event::Key(event) => match event.code {
+                event::KeyCode::Esc => Some(Action::EnterMode(Mode::Normal)),
+                event::KeyCode::Enter => Some(Action::NewLine),
+                event::KeyCode::Char(c) => Some(Action::AddChar(c)),
+                _ => None,
+            },
+            _ => None,
+        };
+
+        Ok(action)
+    }
+}
