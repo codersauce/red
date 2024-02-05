26e8a1c9f18e031c8d97ab465644870d936b6202
Felipe Coury <felipe.coury@gmail.com>
2024-02-05 00:14:44 -0300

    added support for buffer and viewport

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
+use std::{io::stdout, panic};
+
+use buffer::Buffer;
+use crossterm::{terminal, ExecutableCommand};
 use editor::Editor;
 
+mod buffer;
 mod editor;
 
 fn main() -> anyhow::Result<()> {
-    let mut editor = Editor::new()?;
-    editor.run()
+    let file = std::env::args().nth(1);
+    let buffer = Buffer::from_file(file);
+    let mut editor = Editor::new(buffer)?;
+
+    panic::set_hook(Box::new(|info| {
+        _ = stdout().execute(terminal::LeaveAlternateScreen);
+        _ = terminal::disable_raw_mode();
+
+        eprintln!("{}", info);
+    }));
+
+    editor.run()?;
+    editor.cleanup()
 }
