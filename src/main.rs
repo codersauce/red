2d11e64af021f7f37c6551cdc8ea5cee32bea70c
Felipe Coury <felipe.coury@gmail.com>
2024-02-05 16:02:39 -0300

    add logger

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,24 @@ use std::{io::stdout, panic};
 use buffer::Buffer;
 use crossterm::{terminal, ExecutableCommand};
 use editor::Editor;
+use logger::Logger;
+use once_cell::sync::OnceCell;
 
 mod buffer;
 mod editor;
+mod logger;
+
+static LOGGER: OnceCell<Logger> = OnceCell::new();
+
+#[macro_export]
+macro_rules! log {
+    ($($arg:tt)*) => {
+        {
+            let log_message = format!($($arg)*);
+            $crate::LOGGER.get_or_init(|| $crate::Logger::new("red.log")).log(&log_message);
+        }
+    };
+}
 
 fn main() -> anyhow::Result<()> {
     let file = std::env::args().nth(1);
