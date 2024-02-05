e75b61374a0313448a537eb56ddacaa0331bda12
Felipe Coury <felipe.coury@gmail.com>
2024-02-05 16:37:22 -0300

    movements and line editing

--- src/buffer.rs
+++ src/buffer.rs
@@ -28,4 +28,22 @@ impl Buffer {
     pub fn len(&self) -> usize {
         self.lines.len()
     }
+
+    pub fn insert(&mut self, x: u16, y: u16, c: char) {
+        if let Some(line) = self.lines.get_mut(y as usize) {
+            (*line).insert(x as usize, c);
+        }
+    }
+
+    pub fn remove(&mut self, x: u16, y: u16) {
+        if let Some(line) = self.lines.get_mut(y as usize) {
+            (*line).remove(x as usize);
+        }
+    }
+
+    pub fn remove_line(&mut self, line: u16) {
+        if self.len() > line as usize {
+            self.lines.remove(line as usize);
+        }
+    }
 }
