14526cc1c3d3bddb886c40119a8a6816c80749b2
Felipe Coury <felipe.coury@gmail.com>
2024-02-10 07:29:11 -0800

    syntax highlighting from vscode theme (#1)
    * extracted VSCode theme parsing to its own file
    
    * extracted VSCode theme parsing to its own file
    
    * checkpoint: vscode theme parsing working
    
    * cleaned up duplicated treesitter capture code
    
    * styles instead of colors for syntax highlighting
    
    * initial translation of vscode scopes to treesitter
    
    * removed unused variables
    
    * better mapping from VSCode to Tree-sitter scopes

---
+++ src/theme/mod.rs
@@ -0,0 +1,66 @@
+use crossterm::style::{Attribute, Attributes, Color, ContentStyle};
+
+mod vscode;
+
+pub use vscode::parse_vscode_theme;
+
+#[derive(Debug)]
+pub struct Theme {
+    pub name: String,
+    pub style: Style,
+    pub token_styles: Vec<TokenStyle>,
+}
+
+impl Theme {
+    pub fn get_style(&self, scope: &str) -> Option<Style> {
+        self.token_styles.iter().find_map(|ts| {
+            if ts.scope.contains(&scope.to_string()) {
+                Some(ts.style.clone())
+            } else {
+                None
+            }
+        })
+    }
+}
+
+#[derive(Debug)]
+pub struct TokenStyle {
+    pub name: Option<String>,
+    pub scope: Vec<String>,
+    pub style: Style,
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct Style {
+    pub fg: Option<Color>,
+    pub bg: Option<Color>,
+    pub bold: bool,
+    pub italic: bool,
+}
+
+impl Style {
+    pub fn to_content_style(&self, fallback_style: &Style) -> ContentStyle {
+        let foreground_color = match self.fg {
+            Some(fg) => Some(fg),
+            None => fallback_style.fg,
+        };
+        let background_color = match self.bg {
+            Some(bg) => Some(bg),
+            None => fallback_style.bg,
+        };
+        let mut attributes = Attributes::default();
+        if self.italic {
+            attributes.set(Attribute::Italic);
+        }
+        if self.bold {
+            attributes.set(Attribute::Bold);
+        }
+
+        ContentStyle {
+            foreground_color,
+            background_color,
+            attributes,
+            ..Default::default()
+        }
+    }
+}
