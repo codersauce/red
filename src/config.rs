d42e8c5fe687580c2821674449133c26493bf3d9
Felipe Coury <felipe.coury@gmail.com>
2024-02-10 18:42:53 -0300

    theme and simple keymapping on config

---
+++ src/config.rs
@@ -0,0 +1,66 @@
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+
+use crate::editor::Action;
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(untagged)]
+pub enum KeyAction {
+    Single(Action),
+    Multiple(Vec<Action>),
+    Nested(HashMap<String, KeyAction>),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Keys {
+    #[serde(default)]
+    pub normal: HashMap<String, KeyAction>,
+    #[serde(default)]
+    pub insert: HashMap<String, KeyAction>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Config {
+    pub keys: Keys,
+    pub theme: String,
+}
+
+#[cfg(test)]
+mod test {
+    use std::fs;
+
+    use crate::editor::Mode;
+
+    use super::*;
+
+    #[test]
+    fn test_persist_config() {
+        let config = Config {
+            theme: "theme/nightfox.json".to_string(),
+            keys: Keys {
+                normal: HashMap::from([
+                    (
+                        "o".to_string(),
+                        KeyAction::Single(Action::InsertLineBelowCursor),
+                    ),
+                    (
+                        "i".to_string(),
+                        KeyAction::Single(Action::EnterMode(Mode::Normal)),
+                    ),
+                ]),
+                insert: HashMap::new(),
+            },
+        };
+
+        let toml = toml::to_string(&config).unwrap();
+        println!("{toml}");
+    }
+
+    #[test]
+    fn test_parse_config() {
+        let toml = fs::read_to_string("src/fixtures/config.toml").unwrap();
+        let config: Config = toml::from_str(&toml).unwrap();
+        println!("{config:#?}");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::{io::stdout, panic};
+use std::{fs, io::stdout, panic};
 
 use buffer::Buffer;
+use config::Config;
 use crossterm::{terminal, ExecutableCommand};
 use editor::Editor;
 use logger::Logger;
 use once_cell::sync::OnceCell;
 
 mod buffer;
+mod config;
 mod editor;
 mod logger;
 mod theme;
@@ -25,11 +27,14 @@ macro_rules! log {
 }
 
 fn main() -> anyhow::Result<()> {
+    let toml = fs::read_to_string("config.toml")?;
+    let config: Config = toml::from_str(&toml)?;
+
     let file = std::env::args().nth(1);
     let buffer = Buffer::from_file(file);
 
-    let theme = theme::parse_vscode_theme("themes/frappe.json")?;
-    let mut editor = Editor::new(theme, buffer)?;
+    let theme = theme::parse_vscode_theme(&config.theme)?;
+    let mut editor = Editor::new(config, theme, buffer)?;
 
     panic::set_hook(Box::new(|info| {
         _ = stdout().execute(terminal::LeaveAlternateScreen);
