3306c90e1ed66b3e33f6e659029a6a935ed33898
Felipe Coury <felipe.coury@gmail.com>
2024-02-10 12:22:54 -0300

    better mapping from VSCode to Tree-sitter scopes

--- src/theme/vscode.rs
+++ src/theme/vscode.rs
@@ -1,10 +1,54 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 use crossterm::style::Color;
+use once_cell::sync::Lazy;
 use serde::Deserialize;
 use serde_json::{Map, Value};
 
 use super::{Style, Theme, TokenStyle};
+
+static SYNTAX_HIGHLIGHTING_MAP: Lazy<HashMap<&'static str, &'static str>> = Lazy::new(|| {
+    let mut m = HashMap::new();
+
+    m.insert("constant", "constant");
+    m.insert("entity.name.type", "type");
+    m.insert("support.type", "type");
+    m.insert("entity.name.function.constructor", "constructor");
+    m.insert("variable.other.enummember", "constructor");
+    m.insert("entity.name.function", "function");
+    m.insert("meta.function-call", "function");
+    m.insert("entity.name.function.member", "function.method");
+    m.insert("variable.function", "function.method");
+    m.insert("entity.name.function.macro", "function.macro");
+    m.insert("support.function.macro", "function.macro");
+    m.insert("variable.other.member", "property");
+    m.insert("variable.other.property", "property");
+    m.insert("variable.parameter", "variable.parameter");
+    m.insert("entity.name.label", "label");
+    m.insert("comment", "comment");
+    m.insert("punctuation.definition.comment", "comment");
+    m.insert("punctuation.section.block", "punctuation.bracket");
+    m.insert("punctuation.definition.brackets", "punctuation.bracket");
+    m.insert("punctuation.separator", "punctuation.delimiter");
+    m.insert("punctuation.accessor", "punctuation.delimiter");
+    m.insert("keyword", "keyword");
+    m.insert("keyword.control", "keyword");
+    m.insert("support.type.primitive", "type.builtin");
+    m.insert("keyword.type", "type.builtin");
+    m.insert("variable.language", "variable.builtin");
+    m.insert("support.variable", "variable.builtin");
+    m.insert("string.quoted.double", "string");
+    m.insert("string.quoted.single", "string");
+    m.insert("constant.language", "constant.builtin");
+    m.insert("constant.numeric", "constant.builtin");
+    m.insert("constant.character", "constant.builtin");
+    m.insert("constant.character.escape", "escape");
+    m.insert("keyword.operator", "operator");
+    m.insert("storage.modifier.attribute", "attribute");
+    m.insert("meta.attribute", "attribute");
+
+    m
+});
 
 pub fn parse_vscode_theme(file: &str) -> anyhow::Result<Theme> {
     let contents = fs::read_to_string(file)?;
@@ -93,12 +137,10 @@ impl TryFrom<VsCodeTokenColor> for TokenStyle {
 }
 
 fn translate_scope(vscode_scope: String) -> String {
-    if vscode_scope == "meta.function-call.constructor".to_string() {
-        return "constructor".to_string();
-    }
-    if vscode_scope == "meta.annotation.rust".to_string() {
-        return "attribute".to_string();
-    }
+    let vscode_scope = SYNTAX_HIGHLIGHTING_MAP
+        .get(&vscode_scope.as_str())
+        .map(|s| s.to_string())
+        .unwrap_or(vscode_scope);
 
     return vscode_scope;
 }
