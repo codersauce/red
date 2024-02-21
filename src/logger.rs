2443a7ba17c6c4193e05b75c9164c92c384a2b54
Hunar Roop Kahlon <hunar.roop@gmail.com>
2024-02-21 17:33:30 +0530

    refactor(logger): create the file correctly
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- src/logger.rs
+++ src/logger.rs
@@ -6,10 +6,13 @@ use std::{
 };
 
 pub fn init() -> anyhow::Result<()> {
-    let log_file = Box::new(tempfile::Builder::new().prefix("red").suffix(".log").append(true).tempfile()?.into_file());
-    let env = env_logger::Env::new().filter_or("RED_LOG", "warn").write_style_or("RED_STYLE", "auto");
+    let (log_file, _) = tempfile::Builder::new().prefix("red-")
+        .rand_bytes(8).suffix(".log").append(true).tempfile()?.keep()?;
 
-    Ok(env_logger::Builder::from_env(env).target(env_logger::Target::Pipe(log_file)).init())
+
+    let env = env_logger::Env::new().filter("RED_LOG").write_style("RED_STYLE");
+
+    Ok(env_logger::Builder::from_env(env).target(env_logger::Target::Pipe(Box::new(log_file))).init())
 }
 
 pub struct Logger {
