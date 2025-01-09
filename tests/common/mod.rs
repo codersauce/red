817a758a01f345bdfee9e66642fcdcae64000ed3
Felipe Coury <felipe.coury@gmail.com>
2025-01-09 00:48:11 -0300

    feat: updated lsp handling code with latest changes

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,43 +1,122 @@
-use red::lsp::{InboundMessage, LspClient};
+use red::lsp::{Diagnostic, InboundMessage, LspClient, LspError, Range};
 use serde_json::Value;
 
+#[derive(Default)]
 pub struct MockLsp;
+
+// Safe to implement Send + Sync since MockLsp is just a unit struct
+unsafe impl Send for MockLsp {}
+unsafe impl Sync for MockLsp {}
 
 #[async_trait::async_trait]
 impl LspClient for MockLsp {
-    async fn initialize(&mut self) -> anyhow::Result<()> {
+    async fn initialize(&mut self) -> Result<(), LspError> {
         Ok(())
     }
 
-    async fn did_open(&mut self, _file: &str, _contents: &str) -> anyhow::Result<()> {
+    async fn did_open(&mut self, _file: &str, _contents: &str) -> Result<(), LspError> {
         Ok(())
     }
 
-    async fn did_change(&mut self, _file: &str, _contents: &str) -> anyhow::Result<()> {
+    async fn did_change(&mut self, _file: &str, _contents: &str) -> Result<(), LspError> {
         Ok(())
     }
 
-    async fn hover(&mut self, _file: &str, _x: usize, _y: usize) -> anyhow::Result<i64> {
+    async fn hover(&mut self, _file: &str, _x: usize, _y: usize) -> Result<i64, LspError> {
         Ok(0)
     }
 
-    async fn goto_definition(&mut self, _file: &str, _x: usize, _y: usize) -> anyhow::Result<i64> {
+    async fn goto_definition(
+        &mut self,
+        _file: &str,
+        _x: usize,
+        _y: usize,
+    ) -> Result<i64, LspError> {
         Ok(0)
     }
 
-    async fn send_request(&mut self, _method: &str, _params: Value) -> anyhow::Result<i64> {
+    async fn completion(&mut self, _file: &str, _x: usize, _y: usize) -> Result<i64, LspError> {
         Ok(0)
     }
 
-    async fn send_notification(&mut self, _method: &str, _params: Value) -> anyhow::Result<()> {
+    async fn format_document(&mut self, _file: &str) -> Result<i64, LspError> {
+        Ok(0)
+    }
+
+    async fn document_symbols(&mut self, _file: &str) -> Result<i64, LspError> {
+        Ok(0)
+    }
+
+    async fn code_action(
+        &mut self,
+        _file: &str,
+        _range: Range,
+        _diagnostics: Vec<Diagnostic>,
+    ) -> Result<i64, LspError> {
+        Ok(0)
+    }
+
+    async fn signature_help(&mut self, _file: &str, _x: usize, _y: usize) -> Result<i64, LspError> {
+        Ok(0)
+    }
+
+    async fn document_highlight(
+        &mut self,
+        _file: &str,
+        _x: usize,
+        _y: usize,
+    ) -> Result<i64, LspError> {
+        Ok(0)
+    }
+
+    async fn document_link(&mut self, _file: &str) -> Result<i64, LspError> {
+        Ok(0)
+    }
+
+    async fn document_color(&mut self, _file: &str) -> Result<i64, LspError> {
+        Ok(0)
+    }
+
+    async fn folding_range(&mut self, _file: &str) -> Result<i64, LspError> {
+        Ok(0)
+    }
+
+    async fn workspace_symbol(&mut self, _query: &str) -> Result<i64, LspError> {
+        Ok(0)
+    }
+
+    async fn call_hierarchy_prepare(
+        &mut self,
+        _file: &str,
+        _x: usize,
+        _y: usize,
+    ) -> Result<i64, LspError> {
+        Ok(0)
+    }
+
+    async fn semantic_tokens_full(&mut self, _file: &str) -> Result<i64, LspError> {
+        Ok(0)
+    }
+
+    async fn inlay_hint(&mut self, _file: &str, _range: Range) -> Result<i64, LspError> {
+        Ok(0)
+    }
+
+    async fn send_request(&mut self, _method: &str, _params: Value) -> Result<i64, LspError> {
+        Ok(0)
+    }
+
+    async fn send_notification(&mut self, _method: &str, _params: Value) -> Result<(), LspError> {
         Ok(())
     }
 
-    async fn recv_response(&mut self) -> anyhow::Result<Option<(InboundMessage, Option<String>)>> {
+    async fn recv_response(
+        &mut self,
+    ) -> Result<Option<(InboundMessage, Option<String>)>, LspError> {
         Ok(None)
     }
 }
 
-pub fn mock_lsp() -> Box<dyn LspClient> {
+pub fn mock_lsp() -> Box<dyn LspClient + Send> {
     Box::new(MockLsp)
 }
